8763487c1effebaefd5d2e2895ccef26f5f6ca27
eije <jimmy@dunken.vodka>
2015-02-05 10:17:26 +0100

    bort med orsa

--- header.php
+++ header.php
@@ -11,21 +11,21 @@
 
     <div id="menu">
 	<ul>
-            <li><a href="/orsa/index.php" id="hem">Hem</a></li>
-	    <li><a href="/orsa/bilder.php" id="bilder">Bilder</a></li>
-	    <li><a href="/orsa/orsingar/" id="orsingar">Orsingar</a>
+            <li><a href="/index.php" id="hem">Hem</a></li>
+	    <li><a href="/bilder.php" id="bilder">Bilder</a></li>
+	    <li><a href="/orsingar/" id="orsingar">Orsingar</a>
             	<ul class="sub-menu">
-			<li><a href="/orsa/orsingar/amandus.php" id="amandus">Amandus</a></li>
-			<li><a href="/orsa/orsingar/chile.php" id="chile">Chile</a></li>
-			<li><a href="/orsa/orsingar/kalle.php" id="kalle">Kalle</a></li>
-			<li><a href="/orsa/orsingar/alkisarna.php" id="alkisarna">Alkisarna</a></li>
-			<li><a href="/orsa/orsingar/eije.php" id="eije">Eije</a></li>
+			<li><a href="/orsingar/amandus.php" id="amandus">Amandus</a></li>
+			<li><a href="/orsingar/chile.php" id="chile">Chile</a></li>
+			<li><a href="/orsingar/kalle.php" id="kalle">Kalle</a></li>
+			<li><a href="/orsingar/alkisarna.php" id="alkisarna">Alkisarna</a></li>
+			<li><a href="/orsingar/eije.php" id="eije">Eije</a></li>
 		</ul>
 	     </li>
-	     <li><a href="/orsa/platser/" id="platser">Platser</a>
+	     <li><a href="/platser/" id="platser">Platser</a>
 	     	<ul class="sub-menu">
-	     		<li><a href="/orsa/platser/helikon.php" id="helikon">Helikon</a>
-	     		<li><a href="/orsa/platser/statoil.php" id="statoil">Statoil</a>
+	     		<li><a href="/platser/helikon.php" id="helikon">Helikon</a>
+	     		<li><a href="/platser/statoil.php" id="statoil">Statoil</a>
 	     	</ul>
 	     </li>
         </ul>
